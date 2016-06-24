e4a3c68ae629d750e0d1eddec08bd7441c96b9e8
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-06-24 11:51:50 +0200

    add headers and params

--- src/main/java/com/swisscom/appcloud/defaultapp/java/IndexRoute.java
+++ src/main/java/com/swisscom/appcloud/defaultapp/java/IndexRoute.java
@@ -7,15 +7,24 @@ import spark.ModelAndView;
 import spark.template.jade.JadeTemplateEngine;
 
 import static spark.Spark.get;
+import static spark.Spark.exception;
 import static spark.Spark.staticFiles;
 
 public class IndexRoute {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("message", "Hello World!");
-
         staticFiles.location("/public");
 
-        get("/", (rq, rs) -> new ModelAndView(map, "index"), new JadeTemplateEngine());
+        get("/", (req, res) -> {
+          Map<String, Object> map = new HashMap<>();
+          map.put("envVariables", System.getenv());
+          map.put("reqHeaders", req.headers());
+          map.put("reqParams", req.queryParams());
+
+          return new ModelAndView(map, "index");
+        }, new JadeTemplateEngine());
+
+        exception(Exception.class, (exception, request, response) -> {
+            exception.printStackTrace();
+        });
     }
 }
